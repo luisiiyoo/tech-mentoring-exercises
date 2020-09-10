1d1e2de3ed78c600c15546ed68ab512cef71d83b
Luis Gonzalez Guzman <luisiiyoo@gmail.com>
2020-09-10 11:50:46 -0500

    Turotial NLP_Restaurant example

---
+++ NLP_Restaurant/samples/test1.py
@@ -0,0 +1,9 @@
+import spacy
+nlp = spacy.load('en')
+
+doc = nlp("Tea is healthy and calming, don't you think?")
+
+print(f"Token \t\tLemma \t\tStopword".format('Token', 'Lemma', 'Stopword'))
+print("-"*40)
+for token in doc:
+    print(f"{str(token)}\t\t{token.lemma_}\t\t{token.is_stop}")
