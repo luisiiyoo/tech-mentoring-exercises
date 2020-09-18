739a9f699d65042f9950a97761f468e9f6bcecdb
Luis Gonzalez Guzman <luisiiyoo@gmail.com>
2020-09-18 11:35:47 -0500

    complete documentation for CardGame

--- CardsGame/Card.py
+++ CardsGame/Card.py
@@ -2,6 +2,18 @@ import constants
 
 
 class Card:
+    '''
+    Card class representing the rank and suit
+
+    Args:
+        rank (int): Value that recieves the card 
+        suit (str): Special character of the card, e.g. '♣'
+
+    Attributes:
+        rank (int): Value that recieves the card 
+        suit (str): Special character of the card, e.g. '♣'
+    '''
+
     def __init__(self, rank: int, suit: str):
         self.rank = rank
         self.suit = suit
@@ -10,6 +22,15 @@ class Card:
         return 'rank: {}, suitType: {}'.format(self.rank, self.suit)
 
     def getPrettyCard(self) -> str:
+        '''
+        Converts the Card object to a string, e.g. 'K♣'
+
+        Args:
+            None
+
+        Returns:
+            pretty_card(str): String with the card value and suit 
+        '''
         suit = self.suit
         rank = constants.SPECIAL_RANKS.get(self.rank) if (
             self.rank in constants.SPECIAL_RANKS) else str(self.rank)
--- CardsGame/Deck.py
+++ CardsGame/Deck.py
@@ -7,6 +7,21 @@ import constants
 
 
 class Deck:
+    '''
+    Deck class that contains the collection of cards and methods like shuffle, pop a card, etc.
+
+    Args:
+        num_ranks (int): Number of ranks by suit
+        suits (Dict[str, str]): Dictionary containing the suits, e.g. 'club': '♣'
+        special_ranks (Dict[int, str]): Dictionary of special characters that receive a rank or value, e.g. 13: 'K'
+
+    Attributes:
+        __num_ranks (int): Number of ranks by suit
+        __suits (Dict[str, str]): Dictionary containing the suits, e.g. 'club': '♣'
+        __special_ranks (Dict[int, str]): Dictionary of special characters that receive a rank or value, e.g. 13: 'K'
+        __cards(List[Card]): List of Card objects
+    '''
+
     def __init__(self, num_ranks: int, suits: Dict[str, str], special_ranks: Dict[int, str], cards: List[Card] = []):
         self.__num_ranks = num_ranks
         self.__suits = suits
@@ -15,6 +30,15 @@ class Deck:
             len(cards) > 0) else self.__createListOfCards()
 
     def __createListOfCards(self):
+        '''
+        Creates the collection of cards based on the suits and number of ranks providen at the Deck object creation
+
+        Args:
+            None
+
+        Returns:
+            cards(List[Card]): List of Card objects
+        '''
         cards: List[Card] = []
         for suit_key in self.__suits:
             for rank in range(1, self.__num_ranks + 1):
@@ -30,15 +54,51 @@ class Deck:
         return ','.join(pretty)
 
     def getPrettyDeck(self) -> List[str]:
+        '''
+        Maps the cards list to an array of pretty Cards (string Cards representation)
+
+        Args:
+            None
+
+        Returns:
+            cards(List[Card]): List of Card objects
+        '''
         return [card.getPrettyCard() for card in self.__cards]
 
     def addCards(self, new_cards: List[Card]) -> None:
+        '''
+        Adds a list of cards to Decks bottom
+
+        Args:
+            new_cards(List[Card]): List of cards to add
+
+        Returns:
+            None
+        '''
         self.__cards = self.__cards + new_cards
 
     def popCard(self) -> Card:
+        '''
+        Gets the above card from the deck
+
+        Args:
+            None
+
+        Returns:
+            card(Card): Card obtained from the deck's top
+        '''
         return self.__cards.pop(0)
 
     def shuffleDeck(self) -> None:
+        '''
+        Shuffles the deck randomly
+
+        Args:
+            None
+
+        Returns:
+            None
+        '''
         num_cards = len(self.__cards)
         idx_ranperm = list(numpy.random.permutation(num_cards))
         shuffled_cards = [self.__cards[idx] for idx in idx_ranperm]
@@ -46,14 +106,24 @@ class Deck:
         self.__cards = shuffled_cards
 
     def splitDeck(self) -> Tuple[Deck, Deck]:
+        '''
+        Splits the Deck object into two Decks
+
+        Args:
+            None
+
+        Returns:
+            deck_A(Deck): Deck object for the player A
+            deck_A(Deck): Deck object for the player A
+        '''
         num_cards = len(self.__cards)
         split_idx = math.floor(num_cards / constants.NUM_SPLITS)
 
-        cards_1 = self.__cards[0:split_idx]
-        cards_2 = self.__cards[split_idx:]
+        cards_A = self.__cards[0:split_idx]
+        cards_B = self.__cards[split_idx:]
 
-        deck_1 = Deck(self.__num_ranks, self.__suits,
-                      self.__special_ranks, cards_1)
-        deck_2 = Deck(self.__num_ranks, self.__suits,
-                      self.__special_ranks, cards_2)
-        return (deck_1, deck_2)
+        deck_A = Deck(self.__num_ranks, self.__suits,
+                      self.__special_ranks, cards_A)
+        deck_B = Deck(self.__num_ranks, self.__suits,
+                      self.__special_ranks, cards_B)
+        return (deck_A, deck_B)
