e9cbcf6e959fa75bad23cd54eaaf6578884386d8
Luis Gonzalez Guzman <luisiiyoo@gmail.com>
2020-09-21 15:37:20 -0500

    Working on interactive game

--- CardsGame/InteractiveGame.py
+++ CardsGame/InteractiveGame.py
@@ -1,5 +1,5 @@
 from termcolor import colored, cprint
-from typing import Dict, List
+from typing import Dict, List, Tuple
 from Game import Game
 from Card import Card
 from Deck import Deck
@@ -17,14 +17,37 @@ class InteractiveGame(Game):
         self.num_cards_use = self.num_cards_take - 1
         self.__start_random = 0
         self.__stop_random = constants.NUM_RANKS * (self.num_cards_use)
+        self.target_number = self.__getRandomNumber()
+        self.options_cards_A: List[Card] = []
+        self.options_cards_B: List[Card] = []
 
-    def __getRandomNumber(self):
-        return random.randint(self.__start_random, self.__stop_random)
-
-    def __popNCards(self):
+    def __popNCards(self) -> Tuple[List[Card], List[Card]]:
         poped_cards_A: List[Card] = []
+        poped_cards_B: List[Card] = []
         count = 0
         while(count < self.num_cards_take):
             poped_cards_A.append(self.deck_A.popCard())
             poped_cards_B.append(self.deck_B.popCard())
             count += 1
+        return (poped_cards_A, poped_cards_B)
+
+    def __getRandomNumber(self) -> int:
+        return random.randint(self.__start_random, self.__stop_random)
+
+    def startGame(self):
+        print('ID Game:', self.id_game)
+        if(self.num_turns == 0):
+            print('Starting the game')
+        else:
+            print('The game was already started')
+
+    def play(self):
+        winner = self._getWinner()
+        if(winner is None):
+            if(len(self.options_cards_A) != 0 and len(self.options_cards_B) != 0):
+                print("Waiting for your Cards")
+            else:
+                print("Getting cards")
+                self.options_cards_A, self.options_cards_B = self.__popNCards()
+
+            print("Game ID: ", self.id_game)
